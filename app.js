7acd1a76fdf52b1c97eaf7739dbbc5c48224e141
dennyaprilio <>
2019-08-02 10:53:26 +0700

    refactor to genesys

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var path = require('path');
 
-var apos = require('apostrophe')({
-  shortName: 'apostrophe-boilerplate',
+var genex = require('genesys')({
+  shortName: 'genesys-boilerplate',
 
   // See lib/modules for basic project-level configuration of our modules
   // responsible for serving static assets, managing page templates and
@@ -12,7 +12,7 @@ var apos = require('apostrophe')({
     // Apostrophe module configuration
 
     // Note: most configuration occurs in the respective
-    // modules' directories. See lib/apostrophe-assets/index.js for an example.
+    // modules' directories. See lib/genesys-assets/index.js for an example.
     
     // However any modules that are not present by default in Apostrophe must at
     // least have a minimal configuration here: `moduleName: {}`
@@ -20,7 +20,7 @@ var apos = require('apostrophe')({
     // If a template is not found somewhere else, serve it from the top-level
     // `views/` folder of the project
 
-    'apostrophe-templates': { viewsFolderFallback: path.join(__dirname, 'views') }
+    'genesys-templates': { viewsFolderFallback: path.join(__dirname, 'views') }
 
   }
 });
